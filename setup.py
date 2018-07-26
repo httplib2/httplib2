ec0facb19cb12891e7361a3953f7496152a62c27
Alex Yu <stinky2nine@users.noreply.github.com>
2018-07-26 15:33:32 -0400

    Remove Python 3.3 reference from setup.py

--- setup.py
+++ setup.py
@@ -69,7 +69,6 @@ A comprehensive HTTP client library, ``httplib2`` supports many features left ou
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
