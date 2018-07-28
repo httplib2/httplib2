a28784f4887f7b38aec91aa2efc3d3a5d1d54c7f
Alex Yu <stinky2nine@users.noreply.github.com>
2018-07-28 10:35:54 -0400

    Download/install Google Cloud SDK with App Engine plug-ins and enable tests/test_gae_local.py in all Travis Python runtime environments

--- tests/test_gae_local.py
+++ tests/test_gae_local.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Tests httplib2 on Google App Engine locally.
 
-Requires Python 2.7 and Google App Engine SDK. See
+Requires Python 2.7 and Google Cloud SDK. See
 https://cloud.google.com/appengine/docs/standard/python/download.
 
 Google App Engine Standard Environment only supports Python 2.7 at the moment.
@@ -18,32 +18,21 @@ import pytest
 import sys
 
 # For local testing.
-sys.path.insert(0, "/usr/local/google-cloud-sdk/platform/google_appengine")
-
-try:
-    import dev_appserver
-
-    dev_appserver.fix_sys_path()
-    from google.appengine.ext import testbed
-except ImportError:
-    if os.environ.get("TRAVIS_PYTHON_VERSION") is not None:
-        pytestmark = pytest.mark.skip(
-            "Unable to import Google App Engine SDK. Skipping all tests."
-        )
-    else:
-        raise
-
-# For local testing. Ensure that we are not loading the httplib2 version
-# included in the Google App Engine SDK.
 if os.environ.get("TRAVIS_PYTHON_VERSION") is None:
+    sys.path.insert(0, "/usr/local/google-cloud-sdk/platform/google_appengine")
     _parent_dir = '/'.join(os.path.realpath(__file__).split('/')[:-2])
-    sys.path.insert(0, os.path.join(_parent_dir, "python2"))
+    if sys.version_info < (3,):
+        sys.path.insert(0, os.path.join(_parent_dir, "python2"))
+    else:
+        sys.path.insert(0, os.path.join(_parent_dir, "python3"))
+
+import dev_appserver
+
+dev_appserver.fix_sys_path()
+
+from google.appengine.ext import testbed
 
 
-@pytest.mark.skipif(
-    os.environ.get("TRAVIS_PYTHON_VERSION") not in (None, "2.7"),
-    reason="TODO: Requires further testing with Google SDK & Python 3.",
-)
 class Test(object):
     def setup_method(self):
         self._testbed = None
