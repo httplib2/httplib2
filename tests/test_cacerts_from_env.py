9ef6a3898d304b9d1a774539f12d192b780f4c74
Dirk Boye <dirkboye@gmail.com>
2018-09-27 12:42:02 +0200

    use mock patch for available flags

--- tests/test_cacerts_from_env.py
+++ tests/test_cacerts_from_env.py
@@ -9,15 +9,15 @@ CERTIFI_CERTS_FILE = "unittest_certifi_file"
 CUSTOM_CA_CERTS = "unittest_custom_ca_certs"
 
 
+@mock.patch("httplib2.certs.certifi_available", False)
+@mock.patch("httplib2.certs.custom_ca_locater_available", False)
 def test_certs_file_from_builtin():
-    httplib2.certs.certifi_available = False
-    httplib2.certs.custom_ca_locater_available = False
     assert httplib2.certs.where() == CA_CERTS_BUILTIN
 
 
+@mock.patch("httplib2.certs.certifi_available", False)
+@mock.patch("httplib2.certs.custom_ca_locater_available", False)
 def test_certs_file_from_environment():
-    httplib2.certs.certifi_available = False
-    httplib2.certs.custom_ca_locater_available = False
     os.environ["HTTPLIB2_CA_CERTS"] = ENV_CERTS_FILE
     assert httplib2.certs.where() == ENV_CERTS_FILE
     os.environ["HTTPLIB2_CA_CERTS"] = ""
@@ -26,31 +26,29 @@ def test_certs_file_from_environment():
     assert httplib2.certs.where() == CA_CERTS_BUILTIN
 
 
+@mock.patch("httplib2.certs.certifi_available", True)
+@mock.patch("httplib2.certs.custom_ca_locater_available", False)
 def test_certs_file_from_certifi():
     certifi_mock = mock.MagicMock()
     certifi_mock.return_value = CERTIFI_CERTS_FILE
     httplib2.certs.certifi_where = certifi_mock
-    httplib2.certs.certifi_available = True
-    httplib2.certs.custom_ca_locater_available = False
     assert httplib2.certs.where() == CERTIFI_CERTS_FILE
-    httplib2.certs.certifi_available = False
-    assert httplib2.certs.where() == CA_CERTS_BUILTIN
 
 
+@mock.patch("httplib2.certs.certifi_available", False)
+@mock.patch("httplib2.certs.custom_ca_locater_available", True)
 def test_certs_file_from_custom_getter():
     custom_ca_mock = mock.MagicMock()
     custom_ca_mock.return_value = CUSTOM_CA_CERTS
     httplib2.certs.custom_ca_locater_where = custom_ca_mock
-    httplib2.certs.custom_ca_locater_available = True
-    httplib2.certs.certifi_available = False
     assert httplib2.certs.where() == CUSTOM_CA_CERTS
 
 
+@mock.patch("httplib2.certs.certifi_available", False)
+@mock.patch("httplib2.certs.custom_ca_locater_available", False)
 def test_with_certifi_removed_from_modules():
     if "certifi" in sys.modules:
         del sys.modules["certifi"]
-    httplib2.certs.certifi_available = False
-    httplib2.certs.custom_ca_locater_available = False
     os.environ["HTTPLIB2_CA_CERTS"] = ENV_CERTS_FILE
     assert httplib2.certs.where() == ENV_CERTS_FILE
     os.environ.pop("HTTPLIB2_CA_CERTS")
