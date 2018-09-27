f163f94d413c18996d6fe04c1a19c47db100da66
Dirk Boye <dirkboye@gmail.com>
2018-09-27 06:41:10 +0200

    change module description

--- python2/httplib2/certs.py
+++ python2/httplib2/certs.py
@@ -1,18 +1,4 @@
-"""
-certs
-
-Returns the path to the ca bundle from the environmental variable
-HTTPLIB2_CA_CERTS, or from ca_certs_locater if available, or
-from certifi, if available, or the default CA certificates file
-bundled with httplib2.
-
-Code originally from Requests library by Kenneth Reitz.
-
-"""
-__version__ = "1.0.0"
-__license__ = "MIT"
-__history__ = """
-"""
+"""Utilities for certificate management."""
 
 import os
 
@@ -20,6 +6,7 @@ import os
 certifi_available = False
 try:
     from certifi import where as certifi_where
+
     certifi_available = True
 except ImportError:
     pass
@@ -28,6 +15,7 @@ except ImportError:
 custom_ca_locater_available = False
 try:
     from ca_certs_locater import get as custom_ca_locater_where
+
     custom_ca_locater_available = True
 except ImportError:
     pass
