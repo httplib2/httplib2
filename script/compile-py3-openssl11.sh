7ea85ccab405e23577cc1dbfe2e9a2a517fffc58
Sergey Shepelev <temotor@gmail.com>
2019-06-05 01:48:42 +0500

    TEMP WIP trying to improve script/compile-py3-openssl11.sh

--- script/compile-py3-openssl11.sh
+++ script/compile-py3-openssl11.sh
@@ -1,68 +1,60 @@
-CACHE_DIR=$HOME/.cache
-SSL_INSTALL=$CACHE_DIR/ssl
-PYTHON_INSTALL=$CACHE_DIR/python
+#!not for running standalone, see .travis.yml
 
-#rm -rf $CACHE_DIR/* # uncomment to rebuild
+cache_dir=$HOME/.cache
+install_dir=$cache_dir/py3-openssl11
+python_version="3.7.3"
+openssl_version="1.1.1c"
+cpucount=$(nproc --all)
+export PYTHONDONTWRITEBYTECODE=1
 
-if [ ! -f $CACHE_DIR/python/bin/python ]; then
-  ORIGINAL_DIRECTORY=$(pwd)
-  PYTHON_VERSION="3.7.3"
-  OPENSSL_VERSION="1.1.1c"
-  echo "Installing deps for python3..."
-  sudo cp -v /etc/apt/sources.list /tmp
-  sudo chmod a+rwx /tmp/sources.list
-  dist=$(lsb_release --codename --short)
-  echo "deb-src http://archive.ubuntu.com/ubuntu/ $dist main" >> /tmp/sources.list
-  sudo cp -v /tmp/sources.list /etc/apt
-  sudo apt-get -qq --yes update
-  sudo apt-get -qq --yes install build-essential
-  sudo apt-get -qq --yes build-dep python3
+#rm -rf $cache_dir/* # uncomment to rebuild
 
-  cpucount=$(nproc --all)
-  cd $CACHE_DIR
-
+if [[ $($install_dir/bin/python -V) != "Python $python_version" ]] ; then
+  (
+  mkdir -p /tmp/source
+  cd /tmp/source
   # Compile OpenSSL
-  if [ ! -d openssl-$OPENSSL_VERSION ]; then
-    wget --quiet https://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz
-    echo "Extracting OpenSSL..."
-    tar xf openssl-$OPENSSL_VERSION.tar.gz
-  fi
-  cd openssl-$OPENSSL_VERSION
-  echo "Compiling OpenSSL $OPENSSL_VERSION..."
-  ./config shared --prefix=$SSL_INSTALL
+  wget --quiet https://www.openssl.org/source/openssl-$openssl_version.tar.gz
+  echo "Extracting OpenSSL..."
+  tar xf openssl-$openssl_version.tar.gz
+  cd ./openssl-$openssl_version
+  echo "Compiling OpenSSL $openssl_version..."
+  ./config shared --prefix=$install_dir
   echo "Running make for OpenSSL..."
   make -j$cpucount -s
   echo "Running make install for OpenSSL..."
-  make install > /dev/null
-  export LD_LIBRARY_PATH=$SSL_INSTALL/lib
-  cd ..
+  make install_sw >/dev/null
+  export LD_LIBRARY_PATH=$install_dir/lib
 
+  cd /tmp/source
+  sudo apt install -qq --yes libffi-dev
   # Compile latest Python
-  if [ ! -d Python-$PYTHON_VERSION ]; then
-    wget --quiet https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tar.xz
-    echo "Extracting Python..."
-    tar xf Python-$PYTHON_VERSION.tar.xz
-  fi
-  cd Python-$PYTHON_VERSION
-  echo "Compiling Python $PYTHON_VERSION..."
+  wget --quiet https://www.python.org/ftp/python/$python_version/Python-$python_version.tar.xz
+  echo "Extracting Python..."
+  tar xf Python-$python_version.tar.xz
+  cd ./Python-$python_version
+  echo "Compiling Python $python_version..."
   # Note we are purposefully NOT using optimization flags as they increase compile time 10x
-  conf_flags="--with-openssl=$SSL_INSTALL --enable-shared --prefix=$PYTHON_INSTALL --with-ensurepip=upgrade"
-  ./configure $conf_flags > /dev/null
+  conf_flags="--with-openssl=$install_dir --prefix=$install_dir --with-ensurepip=upgrade"
+  CFLAGS=-O1 ./configure $conf_flags > /dev/null
   make -j$cpucount -s
   echo "Installing Python..."
-  make install > /dev/null
-  ln -s $PYTHON_INSTALL/bin/python3 $PYTHON_INSTALL/bin/python
-  ln -s $PYTHON_INSTALL/bin/pip3 $PYTHON_INSTALL/bin/pip
-  cd $ORIGINAL_DIRECTORY
+  make altinstall bininstall >/dev/null
+  ln -fs pip3.7 $install_dir/bin/pip3
+  ln -fs pip3 $install_dir/bin/pip
+  ln -fs python3 $install_dir/bin/python
+  # care for CI cache size
+  find $install_dir -type d -name __pycache__ -print0 |xargs -0 rm -rf
+  )
 fi
 
-export LD_LIBRARY_PATH=$PYTHON_INSTALL/lib:$SSL_INSTALL/lib
-export PATH=$PYTHON_INSTALL/bin:$SSL_INSTALL/bin:$PATH
-echo "Python version:"
-python -V
-echo "OpenSSL version:"
-openssl version
-echo "Python compiled against OpenSSL version:"
-python -c "import ssl; print(ssl.OPENSSL_VERSION)"
-echo "pip version:"
-pip --version
+export LD_LIBRARY_PATH=$install_dir/lib
+export PATH=$install_dir/bin:$PATH
+if [[ $(python -V) != "Python $python_version" ]] ; then
+  echo "Required Python version was not installed into PATH" >&2
+  exit 1
+fi
+if [[ $(python -c 'import ssl; print(ssl.OPENSSL_VERSION)') != OpenSSL\ ${openssl_version}* ]] ; then
+  echo "Required OpenSSL version was not installed into Python" >&2
+  exit 1
+fi
